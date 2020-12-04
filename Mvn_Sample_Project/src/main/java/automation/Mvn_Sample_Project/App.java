a0208bcb300280f5ed431e7d398438f67dca12e5
AshwiniCM <ashwinicm10@gmail.com>
2020-12-04 22:26:32 +0530

    Intial Framework Draft

---
+++ Mvn_Sample_Project/src/main/java/automation/Mvn_Sample_Project/App.java
@@ -0,0 +1,13 @@
+package automation.Mvn_Sample_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
